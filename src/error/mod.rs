ac9473e4f2052b2e28c9e55994138fa8bed88ce2
Frankie Baffa <frankie.baffa@gentryhealthservices.com>
2021-10-18 07:15:12 -0400

    initial commit

---
+++ src/error/mod.rs
@@ -0,0 +1,39 @@
+use {
+    rusqlite::Error as RusqliteError,
+    std::{
+        error::Error as StdError,
+        fmt::Display as StdDisplay,
+        fmt::Formatter as StdFormatter,
+        fmt::Result as FmtResult,
+    },
+};
+pub trait BuildliteErrorMatch<T, U>: Sized where U: StdError {
+    fn quick_match(self) -> Result<T, BuildliteError>;
+}
+#[derive(Debug)]
+pub enum BuildliteError {
+    NoRowsError,
+    SQLError(RusqliteError),
+}
+impl StdDisplay for BuildliteError {
+    fn fmt(&self, f: &mut StdFormatter) -> FmtResult {
+        match self {
+            BuildliteError::NoRowsError => {
+                write!(f, "No rows found!")
+            },
+            BuildliteError::SQLError(e) => {
+                let msg = &format!("{}", e);
+                f.write_str(msg)
+            },
+        }
+    }
+}
+impl StdError for BuildliteError {}
+impl<T> BuildliteErrorMatch<T, RusqliteError> for Result<T, RusqliteError> {
+    fn quick_match(self) -> Result<T, BuildliteError> {
+        return match self {
+            Ok(s) => Ok(s),
+            Err(e) => Err(BuildliteError::SQLError(e)),
+        };
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,4 @@
+mod query;
+pub use query::Query;
+mod error;
+pub use error::BuildliteError;
